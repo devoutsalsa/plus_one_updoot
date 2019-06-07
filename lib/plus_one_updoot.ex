865cdae32ccf2d3bf6b13e9230283bdf2f53778f
Michael Pope <mpope.cr@gmail.com>
2019-06-07 11:49:20 -0700

    Create initial commit

---
+++ lib/plus_one_updoot.ex
@@ -0,0 +1,18 @@
+defmodule PlusOneUpdoot do
+  @moduledoc """
+  Documentation for PlusOneUpdoot.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> PlusOneUpdoot.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/plus_one_updoot_test.exs
@@ -0,0 +1,8 @@
+defmodule PlusOneUpdootTest do
+  use ExUnit.Case
+  doctest PlusOneUpdoot
+
+  test "greets the world" do
+    assert PlusOneUpdoot.hello() == :world
+  end
+end
